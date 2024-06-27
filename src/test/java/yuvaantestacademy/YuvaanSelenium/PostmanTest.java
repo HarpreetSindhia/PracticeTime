d6577d66921e205b22dfcf6948f2a0f6b761c64f
harpreetsindhia999@gmail.com <harpreetsindhia999@gmail.com>
2024-06-27 17:20:29 +0530

    File Commit by Selenium Sindhia

---
+++ src/test/java/yuvaantestacademy/YuvaanSelenium/PostmanTest.java
@@ -0,0 +1,13 @@
+package yuvaantestacademy.YuvaanSelenium;
+
+import org.testng.annotations.Test;
+
+public class PostmanTest {
+	
+	@Test
+	public void getPostman() {
+		
+		System.out.println("Hey Postman framework");
+	}
+
+}
---
+++ src/test/java/yuvaantestacademy/YuvaanSelenium/SeleniumTest.java
@@ -0,0 +1,17 @@
+package yuvaantestacademy.YuvaanSelenium;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	
+		
+		@Test
+		public void getData()
+		{
+			
+			System.out.println("Hello Selenium jar files");
+		}
+	}
+
+
